32ed881f7f5003be9bd16ea9d6ad9df2d93f0248
Ederson Borges <edersonh.a.b@gmail.com>
2024-10-29 12:01:17 -0300

    refactor: remocao historico migracao

--- prisma/migrations/20240910130022_adicionar_relacao_negociacao_tarefas/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "tarefas" ADD COLUMN     "negociacaoId" TEXT;
-
--- AddForeignKey
-ALTER TABLE "tarefas" ADD CONSTRAINT "tarefas_negociacaoId_fkey" FOREIGN KEY ("negociacaoId") REFERENCES "negociacao"("_id") ON DELETE SET NULL ON UPDATE CASCADE;
